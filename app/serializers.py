37286b57e3d0c069078100039752096926daff09
Mike Kreiser <mkreise2@illinois.edu>
2016-07-05 00:13:01 -0500

    Add serializers

---
+++ app/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from app import Athlete, Event, Meet, Result
+
+class AthleteSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Athlete
+    fields = ('id', 'name', 'gender', 'membership')
+
+class EventSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Event
+    fields = ('id', 'name', 'season', 'relay')
+
+class MeetSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Meet
+    fields = ('id', 'date', 'host', 'location', 'name', 'notes', 'resultURL', 'season', 'splitURL')
+
+class ResultSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Result
+    fields = ('id', 'athlete', 'event', 'result', 'distanceResult')
